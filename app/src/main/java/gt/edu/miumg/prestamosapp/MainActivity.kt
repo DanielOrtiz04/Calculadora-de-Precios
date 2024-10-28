6054aa56ce82dd82e4ff9b5634a9a4fd4df95e42
Daniel <dortizc13@miumg.edu.gt>
2024-10-27 21:11:02 -0600

    Cambios

--- app/src/main/java/gt/edu/miumg/prestamosapp/MainActivity.kt
+++ app/src/main/java/gt/edu/miumg/prestamosapp/MainActivity.kt
@@ -1,11 +1,11 @@
-package gt.edu.miumg.fernan.prestamosapp
+package gt.edu.miumg.prestamosapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import gt.edu.miumg.fernan.prestamosapp.ui.theme.PrestamosAppTheme
-import gt.edu.miumg.fernan.prestamosapp.views.HomeView
+import gt.edu.miumg.prestamosapp.ui.theme.PrestamosAppTheme
+import gt.edu.miumg.prestamosapp.views.HomeView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
